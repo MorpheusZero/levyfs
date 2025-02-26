7fc37cddd1ec446f4c7f9a239db80343f0aec1e7
Dylan Legendre <dylanlegendre09@gmail.com>
2025-02-25 22:14:01 -0600

    finishing first draft

--- levyfs.go
+++ levyfs.go
@@ -6,26 +6,64 @@ type PageFile struct {
 
 type Node struct {
 	Key     string
-	Value   string
+	Value   []byte
 	Children map[string]*Node
+}
+
+func NewPageFile() *PageFile {
+	return &PageFile{
+		RootNode: NewRootNode(),
+	}
 }
 
 func NewRootNode() *Node {
 	return &Node{
 		Key:     "/",
-		Value:   "",
+		Value:   nil,
 		Children: make(map[string]*Node),
 	}
 }
 
 func NewNode(key string, value string) *Node {
+
+	valueBytes := []byte{}
+
+	if value != "" {
+		valueBytes = []byte(value)
+	}
+
 	return &Node{
 		Key:     key,
-		Value:   value,
+		Value:   valueBytes,
 		Children: make(map[string]*Node),
 	}
 }
 
 func (n *Node) AddChildNode(child *Node) {
 	n.Children[child.Key] = child
+}
+
+func (p *PageFile) FindNodeWithKey(key string) *Node {
+	return findNodeRecursive(p.RootNode, key)
+}
+
+func findNodeRecursive(node *Node, key string) *Node {
+	if node.Key == key {
+		return node
+	}
+	for _, child := range node.Children {
+		if found := findNodeRecursive(child, key); found != nil {
+			return found
+		}
+	}
+	return nil
+}
+
+func (p *PageFile) GetStringValueForNodeKey(key string) *string {
+	node := p.FindNodeWithKey(key)
+	if node != nil {
+		var value = string(node.Value)
+		return &value
+	}
+	return nil
 }
--- levyfs_test.go
+++ levyfs_test.go
@@ -1,6 +1,7 @@
 package levyfs
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -11,7 +12,7 @@ func TestNodeCreation(t *testing.T) {
 		t.Errorf("Expected Key to be '/', got %s", rootNode.Key)
 	}
 
-	if rootNode.Value != "" {
+	if rootNode.Value != nil {
 		t.Errorf("Expected Key to be 'EMPTY', got %s", rootNode.Key)
 	}
 
@@ -31,7 +32,92 @@ func TestAddChildNode(t *testing.T) {
 		t.Errorf("Expected 1 child, got %d", len(root.Children))
 	}
 
-	if root.Children["child"].Value != "childValue" {
+	if string(root.Children["child"].Value) != "childValue" {
 		t.Errorf("Expected child Value to be 'childValue', got %s", root.Children["child"].Value)
+	}
+}
+
+func TestAddingMultipleChildNodes(t *testing.T) {
+	root := NewRootNode()
+
+	children := make([]struct {
+		key   string
+		value string
+	}, 1000)
+
+	for i := 0; i < 1000; i++ {
+		children[i] = struct {
+			key   string
+			value string
+		}{
+			key:   fmt.Sprintf("child%d", i+1),
+			value: fmt.Sprintf("childValue%d", i+1),
+		}
+	}
+
+	for _, child := range children {
+		root.AddChildNode(NewNode(child.key, child.value))
+	}
+
+	if len(root.Children) != len(children) {
+		t.Errorf("Expected %d children, got %d", len(children), len(root.Children))
+	}
+
+	for _, child := range children {
+		if string(root.Children[child.key].Value) != child.value {
+			t.Errorf("Expected child Value to be '%s', got %s", child.value, root.Children[child.key].Value)
+		}
+	}
+}
+
+func TestPageFileCreation(t *testing.T) {
+	pageFile := NewPageFile()
+
+	if pageFile.RootNode.Key != "/" {
+		t.Errorf("Expected Key to be '/', got %s", pageFile.RootNode.Key)
+	}
+
+	if pageFile.RootNode.Value != nil {
+		t.Errorf("Expected Key to be 'EMPTY', got %s", pageFile.RootNode.Key)
+	}
+
+	if len(pageFile.RootNode.Children) != 0 {
+		t.Errorf("Expected Children to be empty, got %d", len(pageFile.RootNode.Children))
+	}
+}
+
+func TestFindNodeWithKey(t *testing.T) {
+	pageFile := NewPageFile()
+
+	pageFile.RootNode.AddChildNode(NewNode("child1", "childValue1"))
+	pageFile.RootNode.AddChildNode(NewNode("child2", "childValue2"))
+	pageFile.RootNode.AddChildNode(NewNode("child3", "childValue3"))
+
+	foundNode := pageFile.FindNodeWithKey("child2")
+
+	if foundNode == nil {
+		t.Errorf("Expected to find a node, got nil")
+	}
+
+	if string(foundNode.Value) != "childValue2" {
+		t.Errorf("Expected Value to be 'childValue2', got %s", foundNode.Value)
+	}
+}
+
+func TestGetValueForNodeKey(t *testing.T) {
+	pageFile := NewPageFile()
+
+	pageFile.RootNode.AddChildNode(NewNode("child1", "childValue1"))
+	pageFile.RootNode.AddChildNode(NewNode("child2", "childValue2"))
+	pageFile.RootNode.AddChildNode(NewNode("child3", "childValue3"))
+
+	value := pageFile.GetStringValueForNodeKey("child2")
+
+	if value == nil {
+		t.Errorf("Expected to find a value, got nil")
+	}
+
+	if *value != "childValue2" {
+		t.Errorf("Expected Value to be 'childValue2', got %s", *value)
 	}
 }
